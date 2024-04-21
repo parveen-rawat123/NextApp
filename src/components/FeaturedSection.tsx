361f42d802b3c8b2e21d9551b2b5c12b799463b8
praveen singh <rawatpraveen477@gmail.com>
2024-04-21 12:06:20 +0530

    styled cards

--- src/components/FeaturedSection.tsx
+++ src/components/FeaturedSection.tsx
@@ -1,7 +1,21 @@
+'use client'
 import React from 'react'
-import courseDta from "../data/music_compo.json"
-import Link from 'next/link'
+import courseData from "../data/music_compo.json"
+import Link from 'next/link';
+import { BackgroundGradient } from './ui/background-gradient';
+import Image from 'next/image';
+interface Course{
+    id: number,
+    title: string,
+    slug: string,
+    description: string,
+    price: number,
+    instructor: string,
+    isFeatured:boolean,
+    image: string 
+}
 const FeaturedSection = () => {
+  const featuredcourses = courseData.courses.filter((course:Course) =>course.isFeatured)
   return (
     <div className='py-12 bg-gray-900'>
       <div>
@@ -10,15 +24,36 @@ const FeaturedSection = () => {
         <p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl'>Learn With The Best</p>
         </div>
       </div>
-      <div className='mt-10 '>
-        <div> </div>
+      <div className='mx-16 mt-10 '>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center'> 
+        {featuredcourses.map((cousre:Course)=>(
+          <div key={cousre.id} className='flex justify-center'>
+           <BackgroundGradient className='rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900'>
+            <div className='h-[25rem]'>
+              <Image className='rounded-lg'
+              src={cousre?.image}
+              alt='course'
+              width={400}
+              height={400}
+              />
+              <b className=' text-xl'>{cousre.title}</b>
+              <p>{cousre.slug}</p>
+              <p>{cousre.description}</p>
+              <p>Price : {cousre.price}</p>
+              <p>Instructor : {cousre.instructor}</p>
+               <Link href={'/course'}>Learn More</Link>
+            </div>
+           </BackgroundGradient>
+          </div>
+        ))}
+        </div>
       </div>
       <div className='mt-20 text-center'>
         <Link href={'/courses'}>
           View All Courses
         </Link>
-      </div>
+      </div> 
     </div>
   )
 }
-export default FeaturedSection
+export default FeaturedSection;
